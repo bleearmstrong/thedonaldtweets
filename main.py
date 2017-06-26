4d100ed5e5b63457427b9fca92157880082bc5eb
goodtimeslim <bleearmstrong@gmail.com>
2017-06-25 18:54:04 -0700

    split tweet if over 140 characters

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import praw
 import random
 import tweepy
+import time
+
+
+def tweet_builder(tweet):
+    output_list = list()
+    input_list = tweet.split()
+    tweet_x = ''
+    for i, word in enumerate(input_list):
+        if len(tweet_x + ' ' + word) > 137:
+            output_list.append(tweet_x + '...')
+            tweet_x = word
+        else:
+            tweet_x = tweet_x + ' ' + word
+    output_list.append(tweet_x)
+    return output_list
+
 
 reddit = praw.Reddit(client_id=CLIENT_ID,
                      client_secret=CLIENT_SECRET,
@@ -25,13 +41,21 @@ for comment in this_thread.comments:
         pass
 
 comments.sort(key=lambda x: -x[2])
-this_comment = random.choice(comments[:int(len(comments) * .25)])
-
-tweet = '-'.join([this_comment[1], this_comment[0].name, str(this_comment[2])])
-
 
 auth = tweepy.OAuthHandler(APP_KEY, APP_SECRET)
 auth.set_access_token(ACCESS_TOKEN, SECRET)
 api = tweepy.API(auth)
 
-api.update_status(tweet)
+
+this_comment = random.choice(comments[:int(len(comments) * .1)])
+
+tweet = '-'.join([this_comment[1], this_comment[0].name, str(this_comment[2])]) + ' #the_donald_tweets'
+
+if len(tweet) <= 140:
+    api.update_status(tweet)
+else:
+    split_up_tweet = tweet_builder(tweet)
+    for i, tweet in enumerate(split_up_tweet):
+        print('tweeting part {} of {}'.format(i, len(split_up_tweet)))
+        api.update_status(tweet)
+        time.sleep(5)
